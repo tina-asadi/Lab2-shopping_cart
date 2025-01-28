285c85d8a9c541d08a5c6432a089a6e2ce0c8602
Tina <asad0019@algonquinlive.com>
2025-01-28 09:19:02 -0800

    Using Try Except for Step 1 asking For number of items.

--- shopping_cart.py
+++ shopping_cart.py
@@ -7,3 +7,11 @@ The program uses exception handling to manage invalid inputs.
 """
 print("Welcome to the Shopping Cart Program!")
 
+# Step 1: Ask the user for the number of items
+try:
+    num_of_items=int(input("Please Enter the number Of items you want to buy: "))
+except:
+    print("Invalid input Please enter an integer number!")
+else:
+    print("Thank you! Number of items were successfully added.")
+
