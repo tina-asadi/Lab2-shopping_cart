a500fdabf150467f7053a34929fc4d9f8a2a57f2
Tina <asad0019@algonquinlive.com>
2025-01-28 09:04:36 -0800

    Adding description and personal information

---
+++ shopping_cart.py
@@ -0,0 +1,7 @@
+"""
+Author: Tina Asadi
+Date: 2025-01-26
+This program simulates a simple shopping cart.
+Users can add items, specify quantities, and see the total cost.
+The program uses exception handling to manage invalid inputs.
+"""
